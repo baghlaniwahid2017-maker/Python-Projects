b61ddc15b5b8e95c70293cf06675591ed5ce33c2
Abdul Wahid Baghlani <you@example.comAbdul>
2026-01-06 14:25:51 +0430

    worked till basic math

--- file 1.py
+++ file 1.py
@@ -1,10 +1,23 @@
-firstName = "Abdul Wahid"
-age = 21
-marital_status = True
-fee = 500
-date_of_birth ="2005-03-10"
-print ("firstname: ", firstName)
-print ("age: ", age)
-print ("marital_status: ", marital_status)
-print ("fee")
-print ("date_of_birth")
+#firstName = "Abdul Wahid"
+#age = 21
+#marital_status = True
+#fee = 500
+#date_of_birth ="2005-03-10"
+#print ("firstname: ", firstName)
+#print ("age: ", age)
+#print ("marital_status: ", marital_status)
+#print ("fee")
+#print ("date_of_birth")
+
+num1 = 54
+num2 = 20 
+result = num1 + num2 
+
+print(result)
+
+Name = (input("Add your name: "))
+num1 = int(input("number 1: "))
+num2 = int(input("number 2: "))
+result = num1 * num2 
+print(Name)
+print(result)
