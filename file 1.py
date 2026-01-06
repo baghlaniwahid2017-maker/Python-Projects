b7531c2914a4e78d683aa6dcbe93d71e9e4487c3
Abdul Wahid Baghlani <you@example.comAbdul>
2026-01-06 16:06:01 +0430

    practiced the basic math

--- file 1.py
+++ file 1.py
@@ -9,15 +9,41 @@
 #print ("fee")
 #print ("date_of_birth")
 
-num1 = 54
-num2 = 20 
-result = num1 + num2 
+#num1 = 54
+#num2 = 20 
+#result = num1 + num2 
 
-print(result)
 
-Name = (input("Add your name: "))
-num1 = int(input("number 1: "))
-num2 = int(input("number 2: "))
-result = num1 * num2 
-print(Name)
-print(result)
+# Part Two
+#print(result)
+ 
+#Name = (input("Add your name: "))
+#num1 = int(input("number 1: "))
+#num2 = int(input("number 2: "))
+#result = num1 * num2 
+#print(Name)
+#print(result)
+# second round practice
+
+#num1 =10
+#num2 =939
+#result = num1 * num2
+
+#print(result)
+#worked as per lessons
+Name = "Abdul Wahid"
+age = 33
+maritial_status = True
+fee = 500
+d_o_b = "1993-03-10"
+print("Name")
+print("age")
+print("maritial_status")
+print("fee")
+print(d_o_b)
+
+print("Name: ", Name)
+print("age: ", age)
+print("maritial_status: ", maritial_status)
+print("fee: ", fee)
+print("d_o_b: ", d_o_b)
