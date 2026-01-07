621e722a0f0d61711803ee1d1555aeac0e6f3497
Abdul Wahid Baghlani <you@example.comAbdul>
2026-01-07 15:12:37 +0430

    the loop for while worked

--- file 1.py
+++ file 1.py
@@ -70,16 +70,56 @@
 #    print("Required Number congratulations")
 
 
-areacode = int(input("Enter Code"))
-if areacode == 90:
-    print("Trukey")
-elif areacode == 91:
-    print("India")
-elif areacode == 92:
-    print("Pakistan")
-elif areacode == 93:
-    print("Loy Afghanistan")
-else:
-    print("Try Again Letter !!!")
+#areacode = int(input("Enter Code"))
+#if areacode == 90:
+#    print("Trukey")
+#elif areacode == 91:
+#    print("India")
+#elif areacode == 92:
+#    print("Pakistan")
+#elif areacode == 93:
+#    print("Loy Afghanistan")
+#else:
+#   print("Try Again Letter !!!")
 
 
+# Day 8: Loop سلسله
+# Loop (While)
+#x = 1 (در این فرمول که X+=1 بعد از پرنت بیاید شمار از همان قمیت اکس شروع میشود)
+#while x < 10:
+#    print(x)
+#    x+=1
+#x = 1 (در این فرمول که X+=1 پیش از پرنت بیاید شمار از همان قمیت اکس شروع میشود)
+#x = 1
+#while x < 10:
+#    x+=1
+#    print(x)
+# اگر بخایم بجای یک عدد دو عدد یا سه عد بالای اکس جمع شود
+#x = 1
+#while x < 10:
+#    print(x)
+#    x+=2
+# اگر بخایم به صورت جفت  بیاید
+
+#x = 1
+#while x <10:
+#    if x % 2==0:
+#        print(x)
+#    x+=1
+
+# اگر بخایم به صورت  طاق بیاید
+
+#x = 1
+#while x <10:
+#    if x % 2!=0:
+#        print(x)
+#    x+=1
+
+# اگر از عدد کلان به خورد بخایم پرنت کنیم 
+
+x = 15
+while x > 1:
+    x-=1
+    print(x)
+    
+    
