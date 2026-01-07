6a2424ad35e466420272b784ccdfa1917d2459a2
Abdul Wahid Baghlani <you@example.comAbdul>
2026-01-07 10:33:08 +0430

    Day four task practiced which was control follow

--- file 1.py
+++ file 1.py
@@ -31,19 +31,40 @@
 
 #print(result)
 #worked as per lessons
-Name = "Abdul Wahid"
-age = 33
-maritial_status = True
-fee = 500
-d_o_b = "1993-03-10"
-print("Name")
-print("age")
-print("maritial_status")
-print("fee")
-print(d_o_b)
+#Name = "Abdul Wahid"
+#age = 33
+#maritial_status = True
+#fee = 500
+#d_o_b = "1993-03-10"
+#print("Name")
+#print("age")
+#print("maritial_status")
+#print("fee")
+#print(d_o_b)
 
-print("Name: ", Name)
-print("age: ", age)
-print("maritial_status: ", maritial_status)
-print("fee: ", fee)
-print("d_o_b: ", d_o_b)
+#print("Name: ", Name)
+#print("age: ", age)
+#print("maritial_status: ", maritial_status)
+#print("fee: ", fee)
+#print("d_o_b: ", d_o_b)
+
+
+
+# Day Four Lesson - Control Flow ( شرط ها)
+
+x = 10
+y = 5
+result = x + y
+#print(result)
+
+#if result == 15:
+#    print("Fifteen")
+#if result != 15:
+#    print("Not Fifteen")
+#if result > 14:
+#   print("Greter then fifteen")
+
+#if result != 15:
+#    print("Not the required number")
+#else:
+#    print("Required Number congratulations")
