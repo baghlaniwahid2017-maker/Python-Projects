31a5ab1f82f249431bad39e057a53a54bcf3e183
Abdul Wahid Baghlani <you@example.comAbdul>
2026-01-11 08:37:44 +0430

    Multipilication worked

--- Mulitipilication.py
+++ Mulitipilication.py
@@ -1,1 +1,10 @@
-print("this is abdul wahid Baghlani)
+#ب اعداد با تکرارش 
+#y = int(input("Enter Number for Multipilication: "))
+#len = int(input("Enter length for Multipilication: "))
+#for x in range (1, len + 1):
+#    print (x * y)
+
+y = int(input("Enter Number for Multipilication: "))
+len = int(input("Enter length for Multipilication: "))
+for x in range (1, len + 1):
+    print (y,"*",x, "=", x * y)
