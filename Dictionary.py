04eae93a869ed068a9c41952c3ffd8614d03d554
Abdul Wahid Baghlani <you@example.comAbdul>
2026-01-26 09:25:26 +0430

    The sets and the common methods of add, remove, discard, diffrence and intersections worked

--- Dictionary.py
+++ Dictionary.py
@@ -30,14 +30,26 @@ student = {
 #student.pop("Email")
 #print(student)
 
-# Nested Dictionary 
+# Nested Dictionary ( is used for complex data)
 
-students = {
-    "Ahmad": {"Age": 30, "Grade":95},
-    "Wahid": {"Age": 32, "Grade":99},
-    "Ali": {"Age": 44, "Grade": 88}
-}
+#students = {
+#    "Ahmad": {"Age": 30, "Grade":95},
+#    "Wahid": {"Age": 32, "Grade":99},
+#    "Ali": {"Age": 44, "Grade": 88}
+#}
 
 #print(students["Ahmad"]["Age"])
 #print(students["Wahid"]["Grade"])
-print(students["Ali"]["Age"])
+#print(students["Ali"]["Age"])
+
+
+# Sets
+
+fruits = {"Apple", " Orange", " Banana", "lemon"}
+#fruits.add("Cheery") ( add new set or item)
+#fruits.remove("Apple") ( remove specific set or item)
+#fruits.discard("Apple") ( remove specific ser or item with no error to show)
+fruits2 = {"lemon", " Avocado", "Apple"}
+#print(fruits.union(fruits2)) ( it combine both sets)
+#print(fruits.difference(fruits2)) ( is show the defirences of set one which fruits from set two which is fruits2, it mean those who are in set a and set b, the remaing of a is shown)
+print(fruits.intersection(fruits2))
