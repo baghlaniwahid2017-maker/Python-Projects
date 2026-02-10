22e263de5297f23952c0b5c411c85a1cffa19c2e
Abdul Wahid Baghlani <you@example.comAbdul>
2026-02-10 11:36:21 +0430

    Class basic and class with with __init__ and attributes worked and practiced

--- Class and Object.py
+++ Class and Object.py
@@ -13,9 +13,9 @@
 #   def Toyota(self):
 #        print("4 Runner")
 
-#Object
-#myCar = Vehicle()
-#myCar.Toyota()
+# Object
+# myCar = Vehicle()
+# myCar.Toyota()
 
 
 #class Computer:
@@ -37,29 +37,122 @@
 
 # --init-- Method  and attributes
 
-class Vehicle:
-    def __init__(self, Name, Model, Color,):
+# class Vehicle:
+#     def __init__(self, Name, Model, Color,):
+#         self.Name = Name
+#         self.Model = Model
+#         self.Color = Color
+    
+#     def Car(self):
+#         print(f"{self.Name} Model {self.Model} Color {self.Color}")
+
+# # #Object
+# # myCar = Vehicle("Toyota", " 2026", " Black")
+# # myCar.Car()
+
+
+# class Organization:
+#     def __init__(self, Name, offices, DateofEstablished,):
+#        self. Name = Name
+#        self. offices = offices
+#        self. DateofEstablished = DateofEstablished
+    
+#     def NGO(self):
+#         print(f"Name {self.Name} Provincial offices {self.offices} DateofEstablished {self.DateofEstablished}")
+
+# Org = Organization ("CHA Organization", "Marar, Zabul, Herat and Kandahar", " 1987-10-10")
+# Org.NGO()
+
+
+# inner Class and type of Variables in Class
+
+# Basic Class 
+
+# class Person:
+#     def greet(self):
+#         print(" Hello, Welcome to the inner class and type varialble lesson")
+
+# p = Person ()
+# p.greet ()
+
+# Class with __init__ and attributes 
+
+# class Car:
+#     def __init__(self, Name, model):
+#         self.Name = Name
+#         self.model = model
+#     def info(self):
+#         print(f"This car is {self.Name} and the modle is {self.model}")
+
+# c = Car("Toyotota", 2025)
+# c.info()
+
+# class Book:
+#     def __init__(self, Name, Author, Yearofpulication):
+#         self.Name = Name
+#         self.Author = Author 
+#         self.Yearofpulication = Yearofpulication
+
+#     def info(self):
+#         print(f" The Name of the book is {self.Name} the Author is {self.Author} and the Year of Pulicaiotn is {self.Yearofpulication}")
+    
+# b = Book(" Masnawe", "Baghlani", "2000-10-10")
+# b.info()
+
+
+class Sport:
+    def __init__(self, Name, City, Totalplayer):
         self.Name = Name
-        self.Model = Model
-        self.Color = Color
+        self.City = City
+        self.Totalplayer = Totalplayer
+
+    def team(self):
+        print(f" The team name is {self.Name} , The City is {self.City} and the Total are {self.Totalplayer}")
     
-    def Car(self):
-        print(f"{self.Name} Model {self.Model} Color {self.Color}")
-
-#Object
-myCar = Vehicle("Toyota", " 2026", " Black")
-myCar.Car()
+s = Sport ("Tawana Volyball", "Herat", "6 Players")
+s.team()
 
 
 
-class Organization:
-    def __init__(self, Name, offices, DateofEstablished,):
-       self. Name = Name
-       self. offices = offices
-       self. DateofEstablished = DateofEstablished
-    
-    def NGO(self):
-        print(f"Name {self.Name} Provincial offices {self.offices} DateofEstablished {self.DateofEstablished}")
 
-Org = Organization ("CHA Organization", "Marar, Zabul, Herat and Kandahar", " 1987-10-10")
-Org.NGO()
+
+
+
+
+
+
+
+# polymorphism 
+
+# import math
+
+# class TrigFunction:
+#     def calculate(self, x):
+#         pass   
+
+
+# class Sine(TrigFunction):
+#     def calculate(self, x):
+#         return math.sin(x)
+
+
+# class Cosine(TrigFunction):
+#     def calculate(self, x):
+#         return math.cos(x)
+
+
+# class Tangent(TrigFunction):
+#     def calculate(self, x):
+#         return math.tan(x)
+
+# class Cotangent(TrigFunction):
+#     def calculate(self, x):
+#         return 1 / math.tan(x)
+
+# # Polymorphism in action
+# functions = [Sine(), Cosine(), Tangent(), Cotangent()]
+
+# angle = math.radians(90)   
+
+# for func in functions:
+#     print(func.calculate(angle))
--- Collections .py
+++ Collections .py
@@ -24,17 +24,18 @@
 
 # List Methods and Slicing 
 
-#fruits = ["apple", "orange" , "banana"]
-#print(fruits)
-#print(type(fruits))
+# fruits = ["apple", "orange" , "banana"]
+# print(fruits)
+# print(type(fruits))
 
 # we can use the Append(always at the end), insert (we can tell to add in specific place), and remove methods 
 
-#fruits = ["apple", "orange" , "banana"]
-#print(fruits)
-#fruits.append("strawberry")
-#fruits.insert(2,"mango")
-#fruits.remove("banana")
+# fruits = ["apple", "orange" , "banana"]
+
+# # fruits.append("strawberry")
+# # fruits.insert(2,"mango")
+# fruits.remove("banana")
+# print(fruits)
 
 # Slicing 
 #print(fruits)
@@ -49,8 +50,8 @@
 #print(Squares)
 
 # Conditional list Comprehensive
-#evens = [x for x in range (10) if x % 2 == 0]
-#print ("evens")
+# evens = [x for x in range (10) if x % 2 == 0]
+# print ("evens")
 
 #Identity Vs Equality 
 
@@ -58,12 +59,12 @@ a = [1, 2, 3]
 b = a
 c = [1, 2, 3]
 
-#print(a == b)
-#print (b == a)
-#print (c == a)
-#print (b == c)
+# print(a == b)
+# print (b == a)
+# print (c == a)
+# print (b == c)
 
-#print( a is c)
+print( a is c)
 #print (a is b)
 #print ( b is c)
 
@@ -73,9 +74,9 @@ c = [1, 2, 3]
 #print ( 5 in b)
 #print ( 2, b, a )
 
-print (a is c)
-print ( a == c)
-a. append(8)
-print(a)
-a.insert(3, 55)
-print(a)
+# print (a is c)
+# print ( a == c)
+# a. append(8)
+# print(a)
+# a.insert(3, 55)
+# print(a)
